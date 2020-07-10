6b21fbefee3e7492d25a901bd8bd73f976cf67f8
MillerMC96 <pywang@ucdavis.edu>
2020-07-09 21:49:21 -0700

    more elegant

--- vpn.sh
+++ vpn.sh
@@ -2,12 +2,14 @@
 printf " VPN "
 status=$( piactl get connectionstate )
 
-if [ $status = "Connected" ]
-then
-    printf "✓ "
-elif [ $status = "Connecting"  ]
-then
-    printf "..."
-else
-    printf "x"
-fi
+echo "${status} "
+
+#if [ $status = "Connected" ]
+#then
+#    printf "✓"
+#elif [ $status = "Connecting" ]
+#then
+#    printf "..."
+#else
+#    printf "X"
+#fi
