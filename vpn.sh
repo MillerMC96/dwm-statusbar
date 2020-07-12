b01f1961dcc036cc2a87db209ebf8b87b2904665
MillerMC96 <pywang@ucdavis.edu>
2020-07-11 23:26:23 -0700

    no busy waiting

--- vpn.sh
+++ vpn.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 printf " VPN "
+
+case $BUTTON in
+	1) piactl connect ;;
+        3) piactl disconnect ;;
+esac
+
 status=$( piactl get connectionstate )
 
-echo "${status} "
+while [ $status = "Connecting" ] || [ $status = "Disconnecting" ]
+do
+    status=$( piactl get connectionstate )
+done
+
+pkill -RTMIN+10 dwmblocks
+    
+#    if [ $status = "Connected" ] || [ $status = "Disconnected" ]
+#    then
+#    status=$( piactl get connectionstate )
+#    echo "${status} "
+#    break
+#    fi
+
 
 #if [ $status = "Connected" ]
 #then
