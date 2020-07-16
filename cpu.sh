7203bcc91e57298d41b1a54714f2fa4413bbc96f
MillerMC96 <pywang@ucdavis.edu>
2020-07-16 15:12:26 -0700

    added pop up info with htop and nvtop

--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+case $BUTTON in
+	1) setsid -f "$TERMINAL" -e htop ;;
+esac
+
 printf " CPU "
 sensors | awk '/Tdie:/{print $2}' | sed 's/+//g' | awk '{print $0" "}'
--- gpu.sh
+++ gpu.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+case $BUTTON in
+	1) setsid -f "$TERMINAL" -e nvtop ;;
+esac
+
 status=$( nvidia-smi | grep "Default" | awk '{print $13}' )
 printf " GPU %s \n" "${status}"
